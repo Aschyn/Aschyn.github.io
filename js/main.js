85c0e062bd27e17140b742dd21c49ea65282ad3b
Garrick Griffith <contactgarrickg@gmail.com>
2018-12-04 20:13:04 -0600

    Complete Change Of Portfolio
    New portfolio with entirely new design.

--- js/main.js
+++ js/main.js
@@ -1,44 +1,23 @@
+function openMenu(){
+  $('#menu_open').css('display','none');
+  $('#menu').css('width','100vw');
+  $('.item').css('width','100%');
+  $('.item').css('opacity','1');
+}
+function closeMenu(){
+  $('#menu').css('width','0%');
+  $('#menu_open').css('display','block');
+  $('.item').css('width','0%');
+  $('.item').css('opacity','0');
+}
+// Create the measurement node
+var scrollDiv = document.createElement("div");
+scrollDiv.className = "scrollbar-measure";
+$('body').append(scrollDiv);
 
-function typeName(){
-    const name = "Garrick Griffith";
-    let tempName = ""
-    let count = 0;
-    function loop(){
-      setTimeout(function(){
-        tempName += name[count];
-        $("#writeName").html(tempName);
-        count++;
-        if(count < name.length){
-          loop();
-        }
-      },200);
-    }
-    loop();
-}
-function introEffect(){
-  setTimeout(function(){
-    $("#develop").css("visibility","visible");
-  },1000);
-  setTimeout(function(){
-    $("#learn").css("visibility","visible");
-  },2000);
-  setTimeout(function(){
-    $("#grow").css("visibility","visible");
-  },3000);
-}
-$(document).ready(function(){
-  var tempScroll= window.pageYOffset;
-  $(window).scroll(function(){
-  var scrollNum = window.pageYOffset || $(window).scrollTop();
-    if(scrollNum > tempScroll && scrollNum > 50){
-      $("#globalNav").css("height","0");
-      $("#globalNav ul").css("opacity","0");
-    }
-    else{
-      $("#globalNav").css("height", "5vh");
-      $("#globalNav ul").css("opacity","1");
-    }
-      tempScroll = scrollNum;
-  });
-  //introEffect();
-});
+// Get the scrollbar width
+var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+console.warn(scrollbarWidth); // Mac:  15
+
+// Delete the DIV
+$('body').remove(scrollDiv);
